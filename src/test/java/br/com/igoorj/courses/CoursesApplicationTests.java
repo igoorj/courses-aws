e5b79242720c217a0133f6dbc07f98bdc5419b9b
Igor Goncalves <goncalvesigor67@gmail.com>
2026-05-23 10:40:52 -0300

    first commit

---
+++ src/test/java/br/com/igoorj/courses/CoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igoorj.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
